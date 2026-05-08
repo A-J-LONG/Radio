725b967a31bea914d40637b50094a225a7c691fa
Xman Deed <Ballymacmoy@outlook.com>
2026-05-09 00:17:34 +0100

    Built for windows. Moved contents of TTS_Gen.py into a function in python.py. Using waitress stream to stream instead of flask. Removed unused file

--- Radio.py
+++ Radio.py
@@ -1,18 +1,22 @@
 from cerebras.cloud.sdk import Cerebras
+from cerebras.cloud.sdk.types import chat
 from mutagen.easyid3 import EasyID3
-import random
-import os
-import sys
-import platform
-import time
-import subprocess as SubP
 from flask import Flask, Response
+from waitress import serve
 from queue import Queue
+import subprocess as SubP
 import threading
+import edge_tts
+import platform
+import asyncio
+import random
+import time
+import stat
+import sys
+import os
 
 def LLM(firstSong, lastSong):
-    try :
-    
+    try :    
         with open(APIPATH, "r") as f :
             API = f.read().strip()
     
@@ -32,66 +36,85 @@ def LLM(firstSong, lastSong):
             messages=[
                 {
 
+                    
                     "role": "system",
-                    "content": f"You are a Female radio presenter called Emily, The previous 2 songs played were {firstSong} and {lastSong} in that order, do not describe your actions only your words, do not make up what is coming up next you dont know",
-                    "name": "listeners"
+                    "content": f"You are a Female radio presenter called Emily, The previous 2 songs played were {firstSong} and {lastSong} in that order, do not describe your actions only your words, do not make up what is coming up next you dont know"
+                     
                 }
         ],
             model="llama3.1-8b",
         )
-        
         return(chat_completion.choices[0].message.content)
+        
          
     except Exception as e:
-        error_code = e.response.status_code
-        if error_code == 401:
+
+        try:
+            error_code = e.response.status_code
+            if error_code == 401:
+                    
+                print("ERROR 401 INVALID API KEY")
             
-            print("ERROR 401 INVALID API KEY")
-    
-            API = input("Input API key").strip()
-
-            with open(APIPATH, "w") as f :
-                f.write(API)
-            
-            return(LLM())
-        elif error_code == 429:
-            return("Cereberus is facing high traffic right now please try again later")
-
-        elif error_code != 200:
-            print(f"ERROR CODE : {error_code}")
-            return("AN UNKNOWN ERROR OCCURED")
-
-def streamOutput(LOCATION) :
+                API = input("Input API key").strip()
         
+                with open(APIPATH, "w") as f :
+                        f.write(API)
+                    
+                return(LLM())
+            elif error_code == 429:
+                return("Cereberus is facing high traffic right now please try again later")
+    
+            elif error_code != 200:
+                print(f"ERROR CODE : {error_code}")
+                return("AN UNKNOWN ERROR OCCURED")
+        except :
+                print(f"Error Code Type : {type(e)}")
+                print(f"Error Details : {e}")
+                return("AN ERROR OCCURED")
+
+async def ttsAudioGeneration(text):
+
+    communication = edge_tts.Communicate(text, "en-IE-EmilyNeural", rate="+15%")
+
+    await communication.save(SPEECHPATH)
+
+def streamOutput(LOCATION, Metadata) :
+
+    print(f"FFMPEG STARTED : {LOCATION}")
+
+    song_title = Metadata.get("title", ["Unknown"])[0]
+    song_artist = Metadata.get("artist", ["Unknown"])[0]
+
     process = SubP.Popen([
         FFMPEGPATH,
         "-re",
         "-i", LOCATION,
-        "-map", "0:a:0",    
+        "-map", "0:a:0",        
         "-ar", "44100",        
         "-ac", "2",            
         "-acodec", "libmp3lame",
         "-b:a", "192k",
+        "-metadata", f"title={song_title}",
+        "-metadata", f"album={song_artist}",
         "-f", "mp3",         
         "pipe:1"
-    ], stdout=SubP.PIPE, stderr=SubP.DEVNULL)
+    ], stdout=SubP.PIPE, stderr=SubP.DEVNULL, creationflags=SubP.CREATE_NO_WINDOW)
 
     try:
-        while True:
+        while process.poll() is None:
             chunk = process.stdout.read(4096)
-            if not chunk:
-                break
-            yield chunk
-    except:
-        pass
+            
+            if chunk:
+                yield chunk
     finally :
         process.kill()
+        process.wait()
 
 def main() :
     while True:
         
-        musicFiles = [ f for f in os.listdir(MUSICPATH)
-                       if  f != ".gitkeep" ]
+        musicFiles = [ f for f in os.listdir(MUSICPATH) if  f != ".gitkeep" ]
+
         if not musicFiles :
             print (f"NO MUSIC FOUND IN {MUSICPATH}")    
             time.sleep(5)
@@ -99,36 +122,39 @@ def main() :
       
         selectedSong = random.choice(musicFiles)
         selectedSong2 = random.choice(musicFiles)
-    
-        print(selectedSong)
-        print(selectedSong2)
-    
+        
         songPlayed = EasyID3(os.path.join(MUSICPATH, selectedSong))
         songPlayed2 = EasyID3(os.path.join(MUSICPATH, selectedSong2))
-    
+        speechPlayed = {
+                        "artist": "Emily",
+                        "album": "Edge_tts",
+                        "title": "Speech Broadcast"
+                        }                    
+
         SONGPATH = os.path.join(MUSICPATH, selectedSong)
         SONGPATH2 = os.path.join(MUSICPATH, selectedSong2)
-    
-        with open(SCRIPTLOCATION, "w") as f :
-            f.write(LLM(songPlayed, songPlayed2))
+
+        print(f"Now Playing : {songPlayed.get("title",["Unknown"])[0]}")
+        print(f"Up Next : {songPlayed2.get("title",["Unknown"])[0]}")
+
+
+        script = (LLM(songPlayed, songPlayed2))
         
-        SubP.Popen([sys.executable, "TTS_Gen.py"])
+        threading.Thread(target=lambda: asyncio.run(ttsAudioGeneration(script)),daemon=True).start()
   
         newCycle = True
         i = 0
         while i <= 1:
             if newCycle :
-                yield from streamOutput(SONGPATH)
+                yield from streamOutput(SONGPATH, songPlayed)
                 newCycle = False
                 i += 1
-                print(f"\n {i} \n")
             elif not newCycle :
-                yield from streamOutput(SONGPATH2)
+                yield from streamOutput(SONGPATH2,songPlayed2)
                 newCycle = True
                 i += 1
-                print(f"\n {i} \n")
         
-        yield from streamOutput(SPEECHPATH)
+        yield from streamOutput(SPEECHPATH, speechPlayed)
 
 def broadCaster() :
     global currentChunk
@@ -149,7 +175,12 @@ def listener():
             
 #driver code
 
-BASEPATH = os.path.dirname(__file__)
+if getattr(sys, 'frozen', False):
+    BASEPATH = os.path.dirname(sys.executable)
+    BASEPATH = os.path.join(BASEPATH, "..", "..","..")
+else :
+    BASEPATH = os.path.dirname(__file__)
+
 MUSICPATH = os.path.join(BASEPATH, "Audio", "Music")
 SPEECHPATH = os.path.join(BASEPATH, "Audio", "Speech", "Speech.mp3")
 DATAPATH = os.path.join(BASEPATH, "Data")
@@ -158,7 +189,12 @@ SCRIPTLOCATION = os.path.join(DATAPATH, "scripts", "Script.txt")
 if platform.system() == "Windows":
     FFMPEGPATH = os.path.join(BASEPATH, "bin", "FFmpeg", "Windows", "ffmpeg-7.1.1", "bin", "ffmpeg.exe")
 else :
-    FFMPEGPATH = os.path.join(BASEPATH, "bin", "FFmpeg", "Linux", "ffmpeg-git-20240629-amd84-static", "ffmpeg")
+    FFMPEGPATH = os.path.join(BASEPATH, "bin", "FFmpeg", "Linux", "ffmpeg-git-20240629-amd64-static", "ffmpeg")
+
+    try:
+        os.chmod(FFMPEGPATH,os.stat(FFMPEGPATH).st_mode | stat.S_IEXEC)
+    except Exception:
+        raise RuntimeError(f"FFMPEG IS NOT EXECUTABLE PLEASE RUN : chmod +x {FFMPEGPATH}")
 
 audio_Buffer = Queue(maxsize=200)
 currentChunk = b""
@@ -171,4 +207,4 @@ def stream() :
 threading.Thread(target=broadCaster, daemon=True).start()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    serve(app, host="0.0.0.0", port=8080)
